3c2fd7b6ef8b1591b62ce62e802158ebbad34c49
Weiqian-Ni-29 <101934090+Weiqian-Ni-29@users.noreply.github.com>
2022-05-30 16:59:18 +0100

    Add files via upload

---
+++ EMPC_with_2_scenarios/make_new_disturbances.m
@@ -0,0 +1,19 @@
+clear;
+clc;
+T = 50;
+global N;
+%% scenario 1: no disturbance, scenario 2: no disturbance, scenario 3: no disturbance
+% d1 = zeros(2,T+1+N);
+% d2 = zeros(2,T+1+N);
+% d3 = zeros(2,T+1+N);
+
+%% scenario 1: a constant in the middle, scenario 3: no disturbance, scenario 2: a bump in the end
+d1 = zeros(2,T+1+N);
+d2 = zeros(2,T+1+N);
+d3 = zeros(2,T+1+N);
+% d2 is the nominal case
+d1(:,:) = 0;
+d2(:,:) = 0.1;
+
+
+save('multidisturbances.mat','d1','d2','d3');
