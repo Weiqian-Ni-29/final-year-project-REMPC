2f4ba487eb6e34d9204e1b75ac5053323597e745
Weiqian-Ni-29 <101934090+Weiqian-Ni-29@users.noreply.github.com>
2022-06-19 17:55:25 +0100

    Add files via upload

---
+++ real_life_simulation/empc_obj3.m
@@ -0,0 +1,42 @@
+function [fval] = empc_obj3(s)
+    % f = x1(t)^2+x2(t)^2
+    global N w1 w2 w3 w4;
+    nx = 2;
+
+    %% scenario 1
+    Xpred1 = s(nx+1:nx*(N+1));  % predicted states for the first scenario
+    fval1 = 0;
+    for i = 0:N-1
+        f = Xpred1(nx*i+1)^4-4*Xpred1(nx*i+1)^3+Xpred1(nx*i+1)^2 +Xpred1(nx*i+nx)^2;
+        fval1 = fval1 + f;
+    end
+
+    %% scenario 2
+    Xpred2 = s(nx*(N+1)+N+nx+1:2*nx*(N+1)+N); % predicted states for the second scenario
+    fval2 = 0;
+    for i = 0:N-1
+        f = Xpred2(nx*i+1)^4-4*Xpred2(nx*i+1)^3+Xpred2(nx*i+1)^2 +Xpred2(nx*i+nx)^2;
+        fval2 = fval2 + f;
+    end
+
+    %% scenario 3
+    Xpred3 = s(2*(nx*(N+1)+N)+nx+1:2*(nx*(N+1)+N)+nx*(N+1)); % predicted states for the second scenario
+    fval3 = 0;
+    for i = 0:N-1
+        f = Xpred3(nx*i+1)^4-4*Xpred3(nx*i+1)^3+Xpred3(nx*i+1)^2 +Xpred3(nx*i+nx)^2;
+        fval3 = fval3 + f;
+    end
+
+    %% scenario 4
+    %% scenario 3
+    Xpred4 = s(3*(nx*(N+1)+N)+nx+1:3*(nx*(N+1)+N)+nx*(N+1)); % predicted states for the second scenario
+    fval4 = 0;
+    for i = 0:N-1
+        f = Xpred4(nx*i+1)^4-4*Xpred4(nx*i+1)^3+Xpred4(nx*i+1)^2 +Xpred4(nx*i+nx)^2;
+        fval4 = fval4 + f;
+    end
+
+    %% compute the overall cost as a linear combination of both
+    fval = w1*fval1+w2*fval2+w3*fval3+w4*fval4;
+    %fval = max(fval1,max(fval2,max(fval3,fval4));
+end
---
+++ real_life_simulation/make_new_disturbances.m
@@ -0,0 +1,23 @@
+clear;
+clc;
+T = 50;
+global N;
+%% scenario 1: no disturbance, scenario 2: no disturbance, scenario 3: no disturbance
+% d1 = zeros(2,T+1+N);
+% d2 = zeros(2,T+1+N);
+% d3 = zeros(2,T+1+N);
+
+%% scenario 1: a constant in the middle, scenario 3: no disturbance, scenario 2: a bump in the end
+d1 = zeros(2,T+1+N);
+d2 = zeros(2,T+1+N);
+d3 = zeros(2,T+1+N);
+% d1 is the nominal case
+d2(1,:) = 1;
+d2(2,:) = -0.3;
+d3(1,:) = -0.8;
+d3(2,:) = 0.02;
+d4(1,:) = 2;
+d4(2,:) = 1;
+
+
+save('multidisturbances.mat','d1','d2','d3');
